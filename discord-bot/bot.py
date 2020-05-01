a17417e7ebb803a27b36036135b05fd9cf064e58
hofstradsc <hofstradsc@gmail.com>
2020-05-01 00:38:16 +0000

    Updated to test before submitting

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -14,9 +14,8 @@ from datetime import datetime
 load_dotenv();
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='.')
 channel = discord.utils.get(bot.get_all_channels(), guild__name='GardeningApp', name='bot-testing-1')
-greg_id = 219987051376803841
 
 @bot.event
 async def on_ready():
@@ -24,36 +23,17 @@ async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
     members = '\n - '.join([member.name for member in guild.members])
     channels = guild.channels
-    greg2 = guild.get_member(greg_id)
-    #await greg2.create_dm()
-    #await greg2.dm_channel.send('on ready message')
-    print(greg2)
-    print('It is time!')
-    print('I am running on ' + bot.user.name)
-    print('With the ID: ' + str(bot.user.id))
-    print('The Guild is ' + guild.name)
-    print('Guild Members:\n-' + members)
 
 # Information generate in the on_ready does not persist. Have to get it within function
 # and everything. Global ones do persist.
-"""
-@tasks.loop(seconds=5)
-async def water():
-    guild = discord.utils.get(bot.guilds, name=GUILD)
-    user = guild.get_member(greg_id)
-    
-    if user.dm_channel == None:
-        await user.create_dm() 
-    await user.dm_channel.send('Water Plant X')
-"""
 @tasks.loop(seconds = 10)
 async def needs_water():
     guild = discord.utils.get(bot.guilds, name=GUILD)
     discord_user = guild.get_member(greg_id)
     await discord_user.create_dm()
     
-    plant_url = 'http://localhost:3000/plant/all'
-    user_url = 'http://localhost:3000/user/hasGarden'
+    plant_url = '/plant/all'
+    user_url = '/user/hasGarden'
     
     # sending get request and saving the response as response object 
     
@@ -66,7 +46,7 @@ async def needs_water():
     
     for user in user_data['users']:
         #retrieve the user's garden
-        user_garden_request = 'http://localhost:3000/user/myGarden/' + str(user['UserId'])
+        user_garden_request = '/user/myGarden/' + str(user['UserId'])
         user_garden = requests.get(url = user_garden_request)
         user_garden_data = user_garden.json()
 
@@ -91,14 +71,10 @@ async def needs_water():
                 await discord_user.dm_channel.send(str(user['UserId']) + " Water your " + plant_in_garden_info["PlantName"] + " at (" + str(plant_in_garden['PosX']) + "," + str(plant_in_garden['PosY']) + ")") 
             #await discord_user.dm_channel.send(current_time-last_watered_date)
             
-@bot.command()
-async def test(ctx):
-    await ctx.send("test")
-        
     
 @bot.command(name='getAllPlants', help='Should return a message to the user thanking them')
 async def getAllPlants(ctx):
-    URL = "http://localhost:3000/plant/all"
+    URL = "/plant/all"
     r = requests.get(url=URL)
     data = r.json()["plants"]
 
